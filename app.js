0cb9add2cc5729573b07883a9e1c790c95c4be06
Zachary Lopez <zlopez@princeton.edu>
2020-08-11 19:01:22 +0800

    Changed port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');  
 var app = express();  
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 3000);
 var io = require('socket.io')(server);
 const url = require('url');
 const https = require('https');
